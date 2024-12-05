e6b945c66efd57b48d9fcd9615f6f0f4c02b59ca
MaskoMasko <persic.massimo@gmail.com>
2024-12-06 00:08:06 +0100

    admin, user views

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path("", views.homepage)
+    path("", views.homepage, name='index'),
+    path("register/",views.register, name="register"),
+    path("admin-only/", views.admin_only_view, name="admin_only"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponseForbidden
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required, user_passes_test
 # Create your views here.
 def homepage(request):
-    return HttpResponse("HomemadeComm")
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('main:index')
+
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+
+    return render(request, 'registration/register.html', context)
+
+def is_admin_user(user):
+    return user.is_staff
+
+@login_required
+def admin_only_view(request):
+    if not request.user.is_staff:
+        return HttpResponseForbidden("You are not authorized to view this page.")
+    return render(request, "admin_only.html")
